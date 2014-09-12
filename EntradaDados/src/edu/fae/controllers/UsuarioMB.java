1533d8e5382ac607348ef9a199fba34a93c5450f
Eric Rodrigo de Freitas <eric_freitas@hotmail.com>
2014-09-12 14:29:37 -0300

    alterações para entrega exercicios 2

--- EntradaDados/src/edu/fae/controllers/UsuarioMB.java
+++ EntradaDados/src/edu/fae/controllers/UsuarioMB.java
@@ -3,26 +3,32 @@ package edu.fae.controllers;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
+
+import org.primefaces.event.FlowEvent;
 
 import edu.fae.dao.DaoFactory;
 import edu.fae.model.Usuario;
 
-
-@ViewScoped
-@ManagedBean(name="usuarioMB")
+@SessionScoped
+@ManagedBean(name = "usuarioMB")
 public class UsuarioMB {
-	
-	private Usuario usuario = new Usuario();
 
-	public String salvar(){
+	private Usuario usuario = new Usuario();
+	private boolean skip;
+
+	public String salvar() {
 		DaoFactory.getUsuarioDao().save(usuario);
 		usuario = new Usuario();
-		
+
 		return "";
 	}
-	
-	public List<Usuario> findAll(){
+
+	public void resetar() {
+		usuario = new Usuario();
+	}
+
+	public List<Usuario> findAll() {
 		return DaoFactory.getUsuarioDao().findAll();
 	}
 
@@ -32,6 +38,23 @@ public class UsuarioMB {
 
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
+	}
+
+	public String onFlowProcess(FlowEvent event) {
+		if (isSkip()) {
+			setSkip(false);
+			return "confirm";
+		} else {
+			return event.getNewStep();
+		}
+	}
+
+	public boolean isSkip() {
+		return skip;
+	}
+
+	public void setSkip(boolean skip) {
+		this.skip = skip;
 	}
 
 }
